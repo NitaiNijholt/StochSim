60c804979f1b3b72a677a58b6b5238897cc84e37
Boyan Mihaylov <mih.boyan@gmail.com>
2023-11-28 13:32:29 +0100

    MultiServerPriority class extension
    Process time is now calculated before submission of request.

--- Assignment 2/simulation.py
+++ Assignment 2/simulation.py
@@ -37,6 +37,26 @@ class MultiServer:
         self.jobs = pd.DataFrame(columns=["jobID", "arr_time", "proc_time", "leave_time", "wait_delta"])
     
 
+    def get_process_time(self):
+        '''Determines service time depending on current strategy
+        '''
+        if self.deterministic:
+            # Constant process time 
+            process_time = self.mu
+        else:
+            if self.hyper_prob:
+                # Draw from hyper-exponential distribution
+                choose_distribution = np.random.uniform(0, 1)
+                if choose_distribution < self.hyper_prob:
+                    process_time = np.random.exponential(self.beta_service)
+                else:
+                    process_time = np.random.exponential(self.beta_service2)
+            else:
+                # Draw from exponential distribution
+                process_time = np.random.exponential(self.beta_service)
+        return process_time
+
+
     def setup_sim(self, print_progress=False):
         '''Set up simulation. If print_progress is True,
         the simulation events are printed on the go
@@ -52,7 +72,7 @@ class MultiServer:
             # Determine inter-arrival time
             inter_t = np.random.exponential(self.beta_arrival)
 
-            # New job arrival at queue
+            # New job arrival at queue after time inter_t
             job_index = next(job_count)
             yield self.env.timeout(inter_t)
             self.env.process(self.job_request(job_index))
@@ -62,11 +82,16 @@ class MultiServer:
         '''Creates job request at server
         '''
 
+        # Register arrival
         if self.print: print(f"Job {request_id} arrives at queue at time {self.env.now}")
         self.jobs.loc[request_id] = [None, None, None, None, None]
         self.jobs["jobID"].loc[request_id] = "{:04d}".format(request_id)
         self.jobs["arr_time"].loc[request_id] = self.env.now
 
+        # Determine service time
+        process_time = self.get_process_time()
+
+        # Submit request
         with self.processor.request() as request:
             yield request
             
@@ -74,34 +99,54 @@ class MultiServer:
             
             self.jobs["proc_time"].loc[request_id] = self.env.now
             self.jobs["wait_delta"].loc[request_id] = self.env.now - self.jobs["arr_time"].loc[request_id]
-            yield self.env.process(self.job_process())
+            yield self.env.process(self.job_process(process_time))
             if self.print: print(f"Job {request_id} is finished at {self.env.now}")
             self.jobs["leave_time"].loc[request_id]= self.env.now
     
 
-    def job_process(self):
-        '''Processes job for a random time
-        from an exponential distribution
-        with parameter mu
+    def job_process(self, process_time):
+        '''Processes job for a duration equal to process_time
         '''
 
-        # Determine service time
-        if self.deterministic:
-            # Constant process time 
-            process_time = self.mu
-        else:
-            if self.hyper_prob:
-                # Draw from hyper-exponential distribution
-                choose_distribution = np.random.uniform(0, 1)
-                if choose_distribution < self.hyper_prob:
-                    process_time = np.random.exponential(self.beta_service)
-                else:
-                    process_time = np.random.exponential(self.beta_service2)
-            else:
-                # Draw from exponential distribution
-                process_time = np.random.exponential(self.beta_service)
-        
         yield self.env.timeout(process_time)
+
+
+class MultiServerPriority():
+    '''Class for a M/M/n queue system server using job priorities
+    '''
+
+    def __init__(self, env, n, lambda_rate, mu, deterministic=False, hyper_prob=None, mu2=None):
+        '''Extends initialisation function of super-class with a priority-resourse server
+        '''
+        super(MultiServer, self).__init__(self, env, n, lambda_rate, mu, deterministic, hyper_prob, mu2)
+
+        self.processor = simpy.PriorityResource(env, capacity=n)
+
+
+    def job_request(self, request_id):
+        '''Creates job request at server
+        '''
+
+        # Register arrival
+        if self.print: print(f"Job {request_id} arrives at queue at time {self.env.now}")
+        self.jobs.loc[request_id] = [None, None, None, None, None]
+        self.jobs["jobID"].loc[request_id] = "{:04d}".format(request_id)
+        self.jobs["arr_time"].loc[request_id] = self.env.now
+
+        # Determine service time
+        process_time = self.get_process_time()
+
+        # Submit request
+        with self.processor.request(priority=process_time) as request:
+            yield request
+            
+            if self.print: print(f"Job {request_id} is starting to be processed at time {self.env.now}")
+            
+            self.jobs["proc_time"].loc[request_id] = self.env.now
+            self.jobs["wait_delta"].loc[request_id] = self.env.now - self.jobs["arr_time"].loc[request_id]
+            yield self.env.process(self.job_process(process_time))
+            if self.print: print(f"Job {request_id} is finished at {self.env.now}")
+            self.jobs["leave_time"].loc[request_id]= self.env.now
 
 
 # env = simpy.Environment()
